12c4b245fb91ceb3b4de6ba61a164a8679d92a0a
b-rhodes <rhodes.j.ben@gmail.com>
2017-05-04 12:07:05 +1200

    Added some test code
    Choosing the direction in quadrant 2.
    Incomplete.

---
+++ Code/takeAPic.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main(){
+	init();
+	take_picture();
+	save_picture("a.bmp");
+return 0; }
---
+++ Code/testCode.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+int main() {
+	// parameters
+	// higher than this means the pixel is white, lower means it's black.
+	int threshold = 227;
+	
+	
+	
+	init();
+	take_picture();
+	display_picture(5,0);
+	int row[320];
+	for(int i = 0; i < 320; i++) {
+		int brightness = get_pixel(120, i, 3);
+		if(brightness <= threshold) {
+			row[i] = 0;
+		} else {
+			row[i] = 1;
+		}
+	}
+	
+	int numLeft = 0;
+	int numRight = 0;
+	bool left = true;
+	for(int i = 0; i < 320; i++) {
+		if(row[i] = 1) {
+			if(left) {
+				numLeft++;
+			} else {
+				numRight++;
+			}
+		}
+	}
+	
+	// Work out how much to turn!
+	
+return 0;}
