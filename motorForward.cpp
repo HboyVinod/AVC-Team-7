5271080e9f547aaeeb5c24f2ef80fadd4559681c
jarrodchardet <jarrod.kaine.chardet@gmail.com>
2017-05-10 11:16:51 +1200

    Add files via upload
    Basic Code for moving the robot (motors 1 and 2 on.)

---
+++ motorForward.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include "E101.h"
+
+}
+int main() {
+    init();    
+    turn_left();
+    set_motor(1, 200);    
+    sleep1(0, 5000);
+    set_motor(2, 200);    
+    sleep1(0, 5000);
+return 0;}
+
+}
