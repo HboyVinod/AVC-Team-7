2d02306db8973f31fd1885fb128c0c2503fd4ab4
b-rhodes <rhodes.j.ben@gmail.com>
2017-05-30 17:21:35 +1200

    minor changes

--- Code/testCode6.cpp
+++ Code/testCode6.cpp
@@ -352,8 +352,8 @@ int q3 (){ //can be iterated
 			rightWheel = 60;
 			updateSpeeds();
 		} else if (rightTape()){
-			leftWheel = 60;
-			rightWheel = 5;
+			leftWheel = 150;
+			rightWheel = -150;
 			updateSpeeds();
 		} else {
 			backwards1();
